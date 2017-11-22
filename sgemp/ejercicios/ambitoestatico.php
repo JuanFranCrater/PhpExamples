2837b306e2aa423416eec93a14adda1b84e316b2
JuanFCrater <juanfbenitezlopez@gmail.com>
2017-11-22 12:47:21 +0100

    Aiii lmao

---
+++ sgemp/ejercicios/ambitoestatico.php
@@ -0,0 +1,14 @@
+<?php
+
+print " <!DOCTYPE html>
+<html lang=\"es\">
+    <head>
+        <title>Titulo de la WEB</title>
+        <meta charset=\"utf-8\"/>
+    </head>
+    <body> ";
+print "
+    </body>
+</html>
+";
+?>
